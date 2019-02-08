980c9d63127cb75e3f275e6381e03534f95c6da0
niladrem <aurbala@gmail.com>
2019-02-08 11:54:20 +0100

    get_tag_content + read_text

--- findText/extractdata.py
+++ findText/extractdata.py
@@ -2,19 +2,26 @@ from bs4 import BeautifulSoup
 import urllib.request
 
 
-def get_tag_content(text, tag, attr=""):
-    pass
+def get_tag_content(text, tag, classes='', ids=''):
+    soup = BeautifulSoup(text, 'html.parser')
+    tag_content = soup.find_all(tag, class_=classes, ids=ids)
+    return str(tag_content[0]) if tag_content else ''
 
+def read_text(text):
+    soup = BeautifulSoup(text, 'html.parser')
+    return soup.get_text()
 
 def get_page_content(url):
     """Return tuple with HTTP status code and page content"""
     try:
         response = urllib.request.urlopen(url)
-        return (response.code, response.read())
+        return response.code, response.read()
     except urllib.error.HTTPError as e:
-        return (e.code, e.reason)
+        return e.code, e.reason
     except urllib.error.URLError as e:
-        return (-1, e.reason)
+        return -1, e.reason
+    except:
+        return -1, "Unexpected error"
 
 
 def get_list_with_class(text, class_name):
--- tests/extractdata_tests.py
+++ tests/extractdata_tests.py
@@ -17,5 +17,37 @@ class TestGettingPageContent(unittest.TestCase):
         self.assertEqual(404, code)
 
 
+class TestGettingTagContent(unittest.TestCase):
+    @classmethod
+    def getTestText(self):
+        return "<html><div>1</div><div id=\"2\">2</div><p id=\"2\">3</p><p id=\"4\">4</p><div class=\"5 7\">5</div>\
+        <p class=\"5\" id=\"2\">6</p><div class=\"6\"><span id=\"2\">7</span></div></html>"
+
+    def testNormalTag(self):
+        text = self.getTestText()
+        self.assertEqual('<div>1</div>', extractdata.get_tag_content(text, 'div'))
+        self.assertEqual('<p id="2">3</p>', extractdata.get_tag_content(text, 'p'))
+        self.assertEqual('<span id="2">7</span>', extractdata.get_tag_content(text, 'span'))
+
+    def testClassTag(self):
+        text = self.getTestText()
+        self.assertEqual('<p class="5" id="2">6</p>', extractdata.get_tag_content(text, 'p', classes='5'))
+        self.assertEqual('<div class="6"><span id="2">7</span></div>',
+                         extractdata.get_tag_content(text, 'div', classes='6'))
+        self.assertEqual('<div class="5 7">5</div>', extractdata.get_tag_content(text, '', classes='5'))
+        self.assertEqual('<div class="5 7">5</div>', extractdata.get_tag_content(text, '', classes='7'))
+
+    def testIdTag(self):
+        text = self.getTestText()
+        self.assertEqual('<p id="4">4</p>', extractdata.get_tag_content(text, 'p', ids='4'))
+        self.assertEqual('<div id="2">2</div>', extractdata.get_tag_content(text, '', ids='2'))
+        self.assertEqual('<p class="5" id="2">6</p>', extractdata.get_tag_content(text, 'p', ids='2'))
+
+    def testReadText(self):
+        text = self.getTestText()
+        self.assertEqual('1', extractdata.read_text(extractdata.get_tag_content(text, 'div')))
+        self.assertEqual('7', extractdata.read_text(extractdata.get_tag_content(text, 'div', classes='6')))
+
+
 if __name__ == '__main__':
     unittest.main()
