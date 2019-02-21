8f87fd0cb78cb68bdef4d1d3c5802c788f6d37c8
niladrem <aurbala@gmail.com>
2019-02-21 16:41:47 +0100

    redesign + extracting basic data

--- findText/extractdata.py
+++ findText/extractdata.py
@@ -2,15 +2,6 @@ from bs4 import BeautifulSoup
 import urllib.request
 
 
-def get_tag_content(text, tag, classes='', ids=''):
-    soup = BeautifulSoup(text, 'html.parser')
-    tag_content = soup.find_all(tag, class_=classes, ids=ids)
-    return str(tag_content[0]) if tag_content else ''
-
-def read_text(text):
-    soup = BeautifulSoup(text, 'html.parser')
-    return soup.get_text()
-
 def get_page_content(url):
     """Return tuple with HTTP status code and page content"""
     try:
@@ -24,6 +15,46 @@ def get_page_content(url):
         return -1, "Unexpected error"
 
 
-def get_list_with_class(text, class_name):
+def text_to_bs(text):
+    return BeautifulSoup(text, 'html.parser')
+
+
+def get_num_pages(bs):
+    pages = bs.find_all(class_='page')
+    return int(pages[-1].get_text()) if pages else 0
+
+
+def verify_list(bs):
+    song_list = bs.find(class_='ranking-lista')
+    return not song_list.get_text().strip() == ''
+
+
+def get_songs_list(bs):
     pass
 
+
+def get_list_of_similar_authors(author):
+    pass
+
+
+def get_author_page(author):
+    pass
+
+
+def get_list_of_given_author_and_page_num(author, page):
+    url = "https://www.tekstowo.pl/piosenki_artysty," + author + ",alfabetycznie,strona," + str(page) + ".html"
+    text = get_page_content(url)
+    bs = text_to_bs(text)
+    return get_songs_list(bs)
+
+
+def get_whole_songs_list(author):
+    first_page = get_author_page(author)
+    first_page_bs = text_to_bs(first_page)
+
+    pg_num = get_num_pages(first_page_bs)
+    songs_list = []
+    for i in range(1, pg_num + 1):
+        songs_list.extend(get_list_of_given_author_and_page_num(author, i))
+
+    return songs_list
--- tests/extractdata_tests.py
+++ tests/extractdata_tests.py
@@ -3,50 +3,42 @@ from findText import extractdata
 
 
 class TestGettingPageContent(unittest.TestCase):
-    def testWorkingPage(self):
+    def test_working_page(self):
         code, content = extractdata.get_page_content('https://www.google.com/')
         self.assertEqual(code, 200)
         self.assertTrue(len(content) > 0)
 
-    def testURLError(self):
+    def test_url_error(self):
         code, content = extractdata.get_page_content('http://www.google.p/')
-        self.assertEqual(-1, code)
+        self.assertEqual(code, -1)
 
-    def testHTTPError(self):
+    def test_http_error(self):
         code, content = extractdata.get_page_content('https://flippingbook.com/404')
-        self.assertEqual(404, code)
+        self.assertEqual(code, 404)
 
 
-class TestGettingTagContent(unittest.TestCase):
-    @classmethod
-    def getTestText(self):
-        return "<html><div>1</div><div id=\"2\">2</div><p id=\"2\">3</p><p id=\"4\">4</p><div class=\"5 7\">5</div>\
-        <p class=\"5\" id=\"2\">6</p><div class=\"6\"><span id=\"2\">7</span></div></html>"
+class TextExtractingDataFromList(unittest.TestCase):
+    def test_working_page(self):
+        # get page content
+        code, content = extractdata.get_page_content('https://www.tekstowo.pl/piosenki_artysty,kult.html')
+        self.assertEqual(code, 200)
+        bs = extractdata.text_to_bs(content)
+        # verify
+        self.assertEqual(extractdata.verify_list(bs), True)
+        # get pages
+        pg_num = extractdata.get_num_pages(bs)
+        self.assertEqual(pg_num, 8)
 
-    def testNormalTag(self):
-        text = self.getTestText()
-        self.assertEqual('<div>1</div>', extractdata.get_tag_content(text, 'div'))
-        self.assertEqual('<p id="2">3</p>', extractdata.get_tag_content(text, 'p'))
-        self.assertEqual('<span id="2">7</span>', extractdata.get_tag_content(text, 'span'))
-
-    def testClassTag(self):
-        text = self.getTestText()
-        self.assertEqual('<p class="5" id="2">6</p>', extractdata.get_tag_content(text, 'p', classes='5'))
-        self.assertEqual('<div class="6"><span id="2">7</span></div>',
-                         extractdata.get_tag_content(text, 'div', classes='6'))
-        self.assertEqual('<div class="5 7">5</div>', extractdata.get_tag_content(text, '', classes='5'))
-        self.assertEqual('<div class="5 7">5</div>', extractdata.get_tag_content(text, '', classes='7'))
-
-    def testIdTag(self):
-        text = self.getTestText()
-        self.assertEqual('<p id="4">4</p>', extractdata.get_tag_content(text, 'p', ids='4'))
-        self.assertEqual('<div id="2">2</div>', extractdata.get_tag_content(text, '', ids='2'))
-        self.assertEqual('<p class="5" id="2">6</p>', extractdata.get_tag_content(text, 'p', ids='2'))
-
-    def testReadText(self):
-        text = self.getTestText()
-        self.assertEqual('1', extractdata.read_text(extractdata.get_tag_content(text, 'div')))
-        self.assertEqual('7', extractdata.read_text(extractdata.get_tag_content(text, 'div', classes='6')))
+    def test_not_working_page(self):
+        # get page content
+        code, content = extractdata.get_page_content('https://www.tekstowo.pl/piosenki_artysty,klt.html')
+        self.assertEqual(code, 200)
+        bs = extractdata.text_to_bs(content)
+        # verify
+        self.assertEqual(extractdata.verify_list(bs), False)
+        # get pages
+        pg_num = extractdata.get_num_pages(bs)
+        self.assertEqual(pg_num, 0)
 
 
 if __name__ == '__main__':
